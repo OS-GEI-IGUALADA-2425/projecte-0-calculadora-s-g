8ef2f1c2f357534c3db8c7b1efbb261a5b295ee8
AdriaMinguez <ama52@alumnes.udl.cat>
2024-09-22 18:17:46 +0200

    @feat: Implementacio divisio. Closes #5

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+float divisio(double a, double b){
+    return a / b;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
 
+float divisio(double a, double b);
 
 #endif // CALC_H
