4a04168af80785d8417d16eadade0305783297dc
Asier Juárez <asier@debian-asier.local>
2024-09-22 18:08:26 +0200

    @feat: Implementació de la funció de multiplicació

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+float multiplicacio(int a, int b)
+{
+    return a * b;
+}
--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,4 @@
 #ifndef CALC_H
 #define CALC_H
-
-
+float multiplicacio(int a, int b); 
 #endif // CALC_H
