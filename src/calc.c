f029f69862472bb528fddb148a3bebb76ba93d29
AdriaMinguez <ama52@alumnes.udl.cat>
2024-09-22 19:23:06 +0200

    @feat: creo que ya

--- src/calc.c
+++ src/calc.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 #include "calc.h"
+
+float arrel(double a)
+{
+    return sqrt(a);
+}
 float resta(int a, int b)
 {
     return a - b;
--- src/calc.h
+++ src/calc.h
@@ -2,4 +2,5 @@
 #define CALC_H
 float divisio(double a, double b);
 float resta(int a, int b);
+float arrel(double a);
 #endif // CALC_H
